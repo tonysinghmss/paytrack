547bd1d8744dc53f6bce8b3f50676ae88719145b
tonysinghmss <manishankarsngh007@gmail.com>
2016-07-27 23:51:56 +0530

    payer url added

--- paytrack/urls.py
+++ paytrack/urls.py
@@ -19,4 +19,5 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^receiver/', include('receiver.urls')),
+    url(r'^payer/', include('payer.urls')),
 ]
