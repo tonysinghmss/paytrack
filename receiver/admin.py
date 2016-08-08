9a934fafcfc48fb094665c91fe03a63f39bc095e
tonysinghmss <manishankarsngh007@gmail.com>
2016-08-08 22:51:40 +0530

    MemberPaymentDetails in admin console.

--- receiver/admin.py
+++ receiver/admin.py
@@ -1,16 +1,21 @@
 from django.contrib import admin
 
-from models import Member, Team, TeamMember
+from models import Member, Team, TeamMember, MemberPaymentDetails
 
 class TeamMemberInline(admin.TabularInline):
     model = TeamMember
     extra = 1
     
+class MemberPaymentInline(admin.TabularInline):
+    model = MemberPaymentDetails
+    extra = 0    
+    
 class MemberAdmin(admin.ModelAdmin):
-    inlines = (TeamMemberInline,)
+    inlines = (TeamMemberInline, MemberPaymentInline,)
 
 class TeamAdmin(admin.ModelAdmin):
-    inlines = (TeamMemberInline,)    
+    inlines = (TeamMemberInline,)
 
 admin.site.register(Team, TeamAdmin)
 admin.site.register(Member, MemberAdmin)
+
